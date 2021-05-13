6f04395299e8ec49fa26096d2d2e4898970ff884
Kimura Takuma <kimura@unirobot.com>
2021-05-13 18:15:57 +0900

    feat: add commit hash

--- release.sh
+++ release.sh
@@ -1,10 +1,17 @@
 #! /bin/bash
 
 version=$1
+commit=$2
 
 if [ $# = 0 ]; then
     echo required argument
     echo please set version name
+    exit 1
+fi
+
+if [ $# = 1 ]; then
+    echo required argument
+    echo please set commit hash
     exit 1
 fi
 
@@ -14,19 +21,20 @@ if [[ ! $version =~ ^v+[0-9](.[0-9]){1,2}$ ]]; then
 fi
 
 create_git_tag() {
-    git tag ${1}
-    git tag ${1}-staging
-    git tag ${1}-dev
-    git tag ${1}-sandbox
+    git tag -a ${1} ${2}
+    git tag -a stg-${1} ${2}
+    git tag -a dev-${1} ${2}
+    git tag -a sandbox-${1} ${2}
 }
 
 push_git_tag() {
     git push origin ${1}          # for production
-    git push origin ${1}-staging
-    git push origin ${1}-dev
-    git push origin ${1}-sandbox
+    git push origin stg-${1}
+    git push origin dev-${1}
+    git push origin sandbox-${1}
 }
 
-create_git_tag $version
+create_git_tag $version $commit
 push_git_tag $version
+
 
